dbaa9cb74b70584952d29c0b5bc2d0a423bbdcd4
RhysDevalckeneer2 <rhys.devalckeneer@hotmail.be>
2023-11-14 07:32:59 +0100

    first commit

--- src/components/ui/CryptoCard.tsx
+++ src/components/ui/CryptoCard.tsx
@@ -1,4 +1,6 @@
 import { faker } from '@faker-js/faker';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import {
     Chart as ChartJS,
@@ -49,53 +51,79 @@ export const options = {
     },
 };
   
-const labels = ['January', 'February', 'March', 'April'];
+const labels = [
+    "1.0012770618165983",
+    "1.000920693141898",
+];
 
-export const data = {
-    labels,
-    datasets: [
-        {
-            fill: true,
-            label: 'Dataset 2',
-            data: labels.map(() => faker.datatype.number({ min: 0, max: 10 })),
-            borderColor: 'rgb(53, 162, 235)',
-            backgroundColor: (context: any) => {
-                const bgColor = [
-                    'rgba(29, 78, 216, 1)',
-                    'rgba(59, 130, 246, .8)',
-                    'rgba(56, 189, 248, 1)'
-                ];
-                if(!context.chart.chartArea) {
-                    return;
-                }
-                console.log(context.chart.chartArea)
-                const { ctx, data, chartArea: {top, bottom}} = context.chart;
-                const gradientBg = ctx.createLinearGradient(0, top, 0, bottom);
-                gradientBg.addColorStop(0, bgColor[0])
-                gradientBg.addColorStop(1, bgColor[1])
-                gradientBg.addColorStop(1, bgColor[2])
-                return gradientBg;
+const CryptoCard = ({name, imageUrl, value, symbol, mccp}: any) => {
+    const [coinPrice, setCoinPrice] = useState([]);
+
+    const [coinLine, setCoinLine] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("https://api.coinranking.com/v2/coins");
+                const data = response.data.data.coins;
+                setCoinPrice(data.slice(0, 4));
+            } catch (error) {
+              console.error("Error fetching data:", error);
+            }
+        };
+        
+        fetchData();
+    },[])    
+    
+    // console.log(coinPrice)
+    const data = {
+        datasets: [
+            {
+                fill: true,
+                label: 'Dataset 2',
+                data: coinPrice.map(item => item),
+
+                borderColor: 'rgba(14, 165, 233, 1)',
+                backgroundColor: (context: any) => {
+                    const bgColor = [
+                        'rgba(59, 130, 246, .4)',
+                        'rgba(56, 189, 248, .1)'
+                    ];
+                    if(!context.chart.chartArea) {
+                        return;
+                    }
+                    // console.log(context.chart.chartArea)
+                    const { ctx, data, chartArea: {top, bottom}} = context.chart;
+                    const gradientBg = ctx.createLinearGradient(0, top, 0, bottom);
+                    gradientBg.addColorStop(0, bgColor[0])
+                    gradientBg.addColorStop(1, bgColor[1])
+                    return gradientBg;
+                },
+                tension: .42,
+                showLine: true,
+                pointRadius: 0,
             },
-            tension: .42,
-            showLine: false,
-            pointRadius: 0,
-        },
-    ],
-};
-  
-
-const CryptoCard = () => {
+        ],
+    };
   return (
     <div className="bg-white rounded border border-gray-100">
-        <div className="w-full flex items-center px-4 pt-4">
-            <div className="bg-gray-400 w-12 h-12 mr-2 rounded-full"></div>
-            CryptoCard
+        <div className="w-full flex px-4 pt-4">
+            <div className="w-5/6 flex justify-start items-center">
+                <img src={imageUrl} className="w-10 h-10 mr-2 rounded-full" alt="logo"/>
+                <div className="flex-col items-center justify-between">
+                    <p className="font-bold text-xl text-slate-700">{symbol}</p>
+                    <p className="text-slate-400 text-sm">{name}</p>
+                </div>
+            </div>
+            <div className="">
+                <p className="bg-slate-100 py-1 px-2 rounded text-slate-500 text-xs font-bold">{mccp}%</p>
+            </div>
         </div>
         <div className="w-full px-4">
-            <h3 className="font-bold text-slate-700 text-lg my-2">$34.123,27</h3>
+            <h3 className="font-bold text-slate-700 text-lg my-2">${value}</h3>
         </div>
     
-        <Line options={options} data={data} redraw={true} height="20px" width="100%" className="rounded"/>
+        <Line options={options} data={data} redraw={true} height="25px" width="100%" className="rounded"/>
     </div>
   )
 }
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,20 +1,71 @@
+import React, { useEffect, useState } from "react";
 import { Header, Sidebar, CryptoCard } from "../../components"
+import axios from "axios";
 
 const Dashboard = () => {
+    const [data, setData] = useState([]);
+    const [coinPrice, setCoinPrice] = useState([]);
+    const firstFourCoins = data.slice(0, 4);
+    const otherCoins = data.slice(5, 23);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+              // Replace 'YOUR_API_URL' with the actual URL of the API you want to fetch data from
+              const response = await axios.get("https://api.coinranking.com/v2/coins");
+              // console.log(response.data.data.coins)
+              setData(response.data.data.coins);
+              setCoinPrice(response.data.data.coins.sparkline)
+            } catch (error) {
+              console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, [])
+    
+
+
   return (
     <div className="relative">
         <Sidebar />
         <div className="w-10/12 bg-gray-50 absolute top-0 right-0">
             <Header />
             <div className="p-4">
-                <div className="grid grid-cols-4 gap-4">
-                    <CryptoCard />
-                    <CryptoCard />
-                    <CryptoCard />
-                    <CryptoCard />
-                </div>
+                {data ? (
+                    <div className="grid grid-cols-4 gap-4">
+                        {firstFourCoins.map((item) => (
+                            <CryptoCard 
+                                key={item.uuid}
+                                imageUrl={item.iconUrl} 
+                                name={item.name} 
+                                value={item.price} 
+                                symbol={item.symbol}
+                                mccp={item.market_cap_change_percentage_24h}
+                            />
+                        ))}
+                    </div>
+                ) : (
+                    <p>Loading...</p>
+                )}
                 <div className="bg-white border border-gray-100 rounded mt-4">
-                    another test
+                {data ? (
+                    <div className="grid grid-cols-3 gap-4">
+                    {otherCoins.map((item) => (
+                        <div key={item.id} className="p-4 flex justify-between border-b border-slate-100">
+                            <div className="flex flex-row items-center">
+                                <img src={item.iconUrl} className="w-10 h-10"/>
+                                <div className=''>
+                                    <p className="ml-4 font-bold text-slate-700">{item.symbol}</p>
+                                    <p className="ml-4 text-xs text-slate-700">{item.name}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                    </div>
+                ) : (
+                    <p>Loading...</p>
+                )}
                 </div>
             </div>
         </div>
